759065e81cfd6d67258fd0eb78e42a2b9242c018
Dwaraka224 <reddydwarakanath800@gmail.com>
2023-11-21 17:48:58 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def dwaraka(request):
+    return render(request,'dwaraka.html')
---
+++ app2/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def siva(request):
+    return render(request,'siva.html')
